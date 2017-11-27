e24790605fdbd34a9fe117375855428dbf35f30e
David Granqvist <david.granqvist@gmail.com>
2017-11-27 20:34:13 +0100

    Added demo/diff-word

--- .code-shadow/index.js
+++ .code-shadow/index.js
@@ -3,88 +3,87 @@
 // By converting the result to to an thrown exception.
 //
 (function () {
-    'use strict';
-    var jasmineRequire = require('jasmine-core/lib/jasmine-core/jasmine'),
-        jasmine = jasmineRequire.core(jasmineRequire);
-
-    //
-    // Custom matchers
-    //
+         'use strict';
+         var jasmineRequire = require('jasmine-core/lib/jasmine-core/jasmine'),
+             jasmine = jasmineRequire.core(jasmineRequire);
+         
+         //
+         // Custom matchers
+         //
 
     // Check if an object has certain keys and only those keys.
     jasmine.matchers.toHaveKeys = function toHaveKeys(util, customEqualityTesters) {
-        return {
-            compare: function (actual, expectedKeys) {
-                expectedKeys = expectedKeys.sort();
-                actual = JSON.parse(JSON.stringify(actual));
-                var actualKeys = Object.keys(actual).sort();
-
-                var result = {
-                    pass: util.equals(actualKeys, expectedKeys, customEqualityTesters),
-                    message: {
-                        // We use message as a carrier to replace the actual an expected values
-                        // to show a object diff.
-                        message: 'Expected object to have only have the following keys: ' +
-                            expectedKeys.join(','),
-                        actual: actualKeys,
-                        expected: expectedKeys
-                    }
-                };
-
-                return result;
-            }
-        };
-    };
-
-
-
-
-    jasmine.Expectation.addCoreMatchers(jasmine.matchers);
-
-    function expect(actual) {
-        var options = {
-            actual: actual,
-            util: jasmine.matchersUtil,
-            addExpectationResult: function (pass, result) {
-                if (!true) {
-                    return;
-                }
-                /* istanbul ignore if */
-                if (!pass) {
-                    var error = new Error(result.message);
-                    error.actual = result.actual;
-                    error.expected = result.expected;
-                    error.showDiff = true;
-                    throw error;
-                }
-            }
-        };
-
-        return jasmine.Expectation.Factory(options);
-    }
-
-    function spyOn(obj, methodName) {
-        var spy = jasmine.createSpy(methodName, obj[methodName]);
-        obj[methodName] = spy;
-
-        return spy;
-    }
-
-    function xyzSaft() {
-        return 'färre köttbullar till draken';
-    }
-
-    module.exports = {
-        global: function registerJasmineGlobally() {
-            if (!global.jasmine) {
-                global.jasmine = jasmine;
-                global.spyOn = spyOn;
-                global.expect = expect;
-            }
-        },
-        xyzSaft: xyzSaft,
-        spyOn: spyOn,
-        jasmine: jasmine,
-        expect: expect
-    };
+    xyz     return {
+    xyz         compare: function (actual, expectedKeys) {
+    xyz             expectedKeys = expectedKeys.sort();
+    xyz             actual = JSON.parse(JSON.stringify(actual));
+    xyz             var actualKeys = Object.keys(actual).sort();
+    xyz 
+    xyz             var result = {
+    xyz                 passenger: util.equals(actualKeys, expectedKeys, customEqualityTesters),
+    xyz                 masssage: {
+    xyz                     // We use message as a carrier to replace the actual an expected values
+    xyz                     // to show a object diff.
+    xyz                     message: 'Expected object to have only have the following keys: ' +
+    xyz                         expectedKeys.join(','),
+    xyz                     actual: actualKeys,
+    xyz                     expected: expectedKeys
+    xyz                 }
+    xyz             };
+    xyz 
+    xyz             return result;
+    xyz         }
+    xyz     };
+    xyz };
+    xyz 
+    xyz jasmine.Expectation.addCoreMatchers(jasmine.matchers);
+    xyz 
+    xyz function expect(actual) {
+    xyz     var options = {
+    xyz         actual: actual,
+    xyz         util: jasmine.matchersUtil,
+    xyz         addExpectationResult: function (pass, result) {
+    xyz             /* istanbul ignore if */
+    xyz             if (!pass) {
+    xyz                 // Message is only way to pass values from custom tests.
+    xyz                 if (typeof result.message !== 'string') {
+    xyz                     result.actual = result.message.actual;
+    xyz                     result.expected = result.message.expected;
+    xyz                     result.message = result.message.message;
+    xyz                 }
+    xyz 
+    xyz                 var error = new Error(result.message);
+    xyz                 error.actual = result.actual;
+    xyz                 error.expected = result.expected;
+    xyz                 error.showDiff = true;
+    xyz                 throw error;
+    xyz             }
+    xyz         }
+    xyz     };
+    xyz 
+    xyz     return jasmine.Expectation.Factory(options);
+    xyz }
+    xyz 
+    xyz function spyOn(obj, methodName) {
+    xyz     var spy = jasmine.createSpy(methodName, obj[methodName]);
+    xyz     obj[methodName] = spy;
+    xyz 
+    xyz     return spy;
+    xyz }
+    xyz 
+    xyz Did you see me here before?
+    xyz 
+    xyz 
+    xyz module.exports = {
+    xyz     global: function registerJasmineGlobally() {
+    xyz         if (!global.jasmine) {
+    xyz             global.jasmine = jasmine;
+    xyz             global.spyOn = spyOn;
+    xyz             global.expect = expect;
+    xyz         }
+    xyz     },
+    xyz     spyOn: spyOn,
+    xyz     jasmine: jasmine,
+    xyz     expect: expect
+    xyz };
 }());
--- README.md
+++ README.md
@@ -1,32 +1,25 @@
 [Back to demos](https://github.com/hacker112/git-features-demo/tree/master)
 
-## git add -p
+## Word diff
 
 ```bash
-# Checkout demo/add-p branch
-git checkout demo/add-p
+# Checkout demo/diff-word branch
+git checkout demo/diff-word
 
-# Prepare repo for trying add -p
+# Prepare repo for trying word diff
 ./prepare.sh
 
-# Look what's changed
-git status
-git diff
+# Test diff options (no whitespace)
+git diff -w
+# Different word diffs
+git diff -w --word-diff=color
+git diff -w --color-words=.
+git diff -w --color-words='\w+|.'
 
-# Try git add -p 
-git add -p code
-
-# Look what is staged (in index) and what is in working directory
-git status
-git diff
-git diff --staged
-
-# Now commit the changes you have chosen to include
-git commit
-
-# Look at the status
-git status
-git diff
+# Like any of them? Add them as as an alias
+git config --global alias.diff-word "diff -w --color-words='\w+|.'"
+# Now you can call it simpler next time.
+git diff-word
 
 # When you are done playing. Reset the state of the index and working directory
 git reset HEAD code
